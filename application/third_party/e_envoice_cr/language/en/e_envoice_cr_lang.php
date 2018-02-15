4dd5c6583c9c2f7a6c64e4d299bc682018d8fef1
Alonso Mora <a.mora@paralleldevs.com>
2018-02-15 17:30:03 -0600

    Added english translation of module

---
+++ application/third_party/e_envoice_cr/language/en/e_envoice_cr_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang["e_envoice_cr_env"] = "Environment";
+$lang["e_envoice_cr_username"] = "Username";
+$lang["e_envoice_cr_password"] = "Password";
+$lang["e_envoice_cr_id_type"] = "Identification Type";
+$lang["e_envoice_cr_id"] = "Company Id (Hacienda)";
+$lang["e_envoice_cr_name"] = "Company Name (Hacienda)";
+$lang["e_envoice_cr_cert"] = "Certificate File";
+$lang["e_envoice_cr_cert_password"] = "Certificate Password";
+
