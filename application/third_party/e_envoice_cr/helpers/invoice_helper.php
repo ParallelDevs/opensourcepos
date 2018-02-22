485aad0a0fd40946d883bd7a950e3cfcd75e4682
Alonso Mora <a.mora@paralleldevs.com>
2018-02-22 15:36:57 -0600

    Updated xml directory

--- application/third_party/e_envoice_cr/helpers/invoice_helper.php
+++ application/third_party/e_envoice_cr/helpers/invoice_helper.php
@@ -3,13 +3,11 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 function get_invoice_dir() {
-  $invoice_dir = 'e_envoice_cr_invoices';
+  $invoice_dir = APPPATH .'/third_party/e_envoice_cr/xml';
   return $invoice_dir;
 }
 
 function is_invoice_dir_valid() {
-  $ci = & get_instance();
-  $ci->load->helper('directory');
   $invoice_dir = get_invoice_dir();
 
   if (!file_exists($invoice_dir)) {
@@ -20,12 +18,10 @@ function is_invoice_dir_valid() {
 }
 
 function create_invoice_dir() {
-  $ci = & get_instance();
-  $ci->load->helper('directory');
   $invoice_dir = get_invoice_dir();
 
   if (!is_dir($invoice_dir)) {
-    mkdir("./$invoice_dir", 0777, true);
+    mkdir("$invoice_dir", 0777, true);
   }
 
   if (!is_writeable($invoice_dir)) {
