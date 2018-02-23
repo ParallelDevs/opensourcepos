9f62d46b7a68a6e7ee2f2553a6b9da4a8b17a9d4
Alonso Mora <a.mora@paralleldevs.com>
2018-02-23 16:51:21 -0600

    [PF-18]: Updated english translation

--- application/third_party/e_envoice_cr/language/english/e_envoice_cr_lang.php
+++ application/third_party/e_envoice_cr/language/english/e_envoice_cr_lang.php
@@ -5,8 +5,9 @@ $lang["e_envoice_cr_username"] = "Username";
 $lang["e_envoice_cr_password"] = "Password";
 $lang["e_envoice_cr_id_type"] = "Identification Type";
 $lang["e_envoice_cr_id"] = "Company Id (Hacienda)";
-$lang["e_envoice_cr_name"] = "Company Name (Hacienda)";
+$lang["e_envoice_cr_commercial_name"] = "Company Name (Hacienda)";
 $lang["e_envoice_cr_cert"] = "Certificate File";
 $lang["e_envoice_cr_cert_password"] = "Certificate Password";
-$lang["e_envoice_cr_currency_code"] = "Currency code";
+$lang["e_envoice_cr_resolution_number"] = "Resolution Number";
+$lang["e_envoice_cr_resolution_date"] = "Resolution Date";
 
