b29b32890779d967201883eb8080d1b84f7e3128
Alonso Mora <a.mora@paralleldevs.com>
2018-02-23 16:51:47 -0600

    [PF-18]: Added spanish translation

---
+++ application/third_party/e_envoice_cr/language/spanish/e_envoice_cr_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang["e_envoice_cr_env"] = "Ambiente";
+$lang["e_envoice_cr_username"] = "Usuario";
+$lang["e_envoice_cr_password"] = "Contraseña";
+$lang["e_envoice_cr_id_type"] = "Tipo de Identificación";
+$lang["e_envoice_cr_id"] = "Identificación (Hacienda)";
+$lang["e_envoice_cr_commercial_name"] = "Nombre Comercial (Hacienda)";
+$lang["e_envoice_cr_cert"] = "Certificado";
+$lang["e_envoice_cr_cert_password"] = "Contraseña Certificado";
+$lang["e_envoice_cr_resolution_number"] = "Número de Resolución";
+$lang["e_envoice_cr_resolution_date"] = "Fecha de Resolución";
+
