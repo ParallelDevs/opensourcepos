7793b28fbd1a5a4f58acf93cc1d29a319d210efe
Alonso Mora <a.mora@paralleldevs.com>
2018-02-27 23:37:07 -0600

    Renamed function in helper

--- application/third_party/e_envoice_cr/helpers/invoice_helper.php
+++ application/third_party/e_envoice_cr/helpers/invoice_helper.php
@@ -2,59 +2,85 @@
 
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-function get_invoice_dir() {
-  $invoice_dir = APPPATH .'/third_party/e_envoice_cr/xml';
+function get_documents_dir() {
+  $invoice_dir = APPPATH . '/third_party/e_envoice_cr/xml';
   return $invoice_dir;
 }
 
-function is_invoice_dir_valid() {
-  $invoice_dir = get_invoice_dir();
+function is_documents_dir_valid() {
+  $documents_dir = get_documents_dir();
 
-  if (!file_exists($invoice_dir)) {
+  if (!file_exists($documents_dir)) {
     return false;
   }
 
-  return is_readable($invoice_dir) && is_writeable($invoice_dir) ? true : false;
+  return is_readable($documents_dir) && is_writeable($documents_dir) ? true : false;
 }
 
-function create_invoice_dir() {
-  $invoice_dir = get_invoice_dir();
+function create_documents_dirs() {
+  $documents_dir = get_documents_dir();
 
-  if (!is_dir($invoice_dir)) {
-    mkdir("$invoice_dir", 0777, true);
+  if (!is_dir($documents_dir)) {
+    mkdir("$documents_dir", 0777, true);
   }
 
-  if (!is_writeable($invoice_dir)) {
-    return chmod($invoice_dir, 0777);
+  if (!is_writeable($documents_dir)) {
+    return chmod($documents_dir, 0777);
   }
 
-  return is_writeable($invoice_dir);
+  return is_writeable($documents_dir);
 }
 
-function format_invoice_number($value, $final_lenght, $padding_string = '0') {
+function create_document_subfolder($document_type) {
+  $documents_dir = get_documents_dir();
+  $subfolder = $documents_dir . "/$document_type";
+
+  if (!is_dir($subfolder)) {
+    mkdir("$subfolder", 0777, true);
+  }
+
+  if (!is_writeable($subfolder)) {
+    return chmod($subfolder, 0777);
+  }
+
+  return is_writeable($subfolder);
+}
+
+function format_document_number($value, $final_lenght, $padding_string = '0') {
   $input = is_string($value) ? $value : (string) $value;
   $output = str_pad($input, $final_lenght, $padding_string, STR_PAD_LEFT);
   return $output;
 }
 
-function format_invoice_date($date_time) {
+function format_document_date($date_time) {
   $time = strtotime($date_time);
   $date = date('c', $time);
   return $date;
 }
 
-function generate_invoice_key($consecutive_number, $secure_code, $id_user) {
+function generate_document_key($consecutive_number, $secure_code, $id_user) {
   $day = date("d");
   $mouth = date("m");
   $year = date("y");
-  $key = '506' . $day . $mouth . $year . $id_user .$consecutive_number . '1' . $secure_code;
+  $key = '506' . $day . $mouth . $year . $id_user . $consecutive_number . '1' . $secure_code;
   return $key;
 }
 
-function generate_invoice_consecutive($sucursal, $terminal, $doc_type, $invoice_number) {
-  $consecutive_number = format_invoice_number($sucursal, 3);
-  $consecutive_number .= format_invoice_number($terminal, 5);
+function generate_document_consecutive($sucursal, $terminal, $doc_type, $invoice_number) {
+  $consecutive_number = format_document_number($sucursal, 3);
+  $consecutive_number .= format_document_number($terminal, 5);
   $consecutive_number .= $doc_type;
-  $consecutive_number .= format_invoice_number($invoice_number, 10);
+  $consecutive_number .= format_document_number($invoice_number, 10);
   return $consecutive_number;
+}
+
+function get_certificate_file() {
+  $file = FCPATH . 'uploads/certs/e_envoice_cert.p12';
+  return $file;
+}
+
+function is_certificate_valid() {
+  $file = get_certificate_file();
+
+  return is_readable($file);
 }
