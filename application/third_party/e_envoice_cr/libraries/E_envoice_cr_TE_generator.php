0f1d9dc3ab8d2cfa736b522e3f5c2c2f8af66373
Alonso Mora <a.mora@paralleldevs.com>
2018-02-28 14:19:28 -0600

    Updated generator for TE documents

--- application/third_party/e_envoice_cr/libraries/E_envoice_cr_TE_generator.php
+++ application/third_party/e_envoice_cr/libraries/E_envoice_cr_TE_generator.php
@@ -13,12 +13,34 @@ class E_envoice_cr_TE_generator extends E_envoice_cr_document_generator {
   public function __construct($consecutive) {
     parent::__construct($consecutive);
     $this->type = 'TE';
-    $this->file = get_documents_dir() . '/' . $this->type . '-' . $this->key . '.xml';
+    $this->file = get_documents_dir() . '/' . $this->type . '/' . $this->key . '.xml';
     $this->initRootTag();
   }
 
   public function generateXMLDocument(&$general_data, &$receiver, &$emitter, &$items) {
-    
+    $children = array();
+    $root = $this->getRootTag();
+    $children[] = $this->getSimpleTag('Clave', $general_data['key']);
+    $children[] = $this->getSimpleTag('NumeroConsecutivo', $general_data['consecutive']);
+    $children[] = $this->getSimpleTag('FechaEmision', $general_data['date']);
+    $children[] = $this->getEmisorTag($emitter);
+    $children[] = $this->getSimpleTag('CondicionVenta', $general_data['condition']);
+    $children[] = $this->getSimpleTag('PlazoCredito', $general_data['p_credit']);
+    $this->getMedioPagoTag($general_data['pay_types'], $children);
+    $children[] = $this->getDetalleServicioTag($items);
+    $children[] = $this->getResumenFacturaTag($general_data);
+    $children[] = $this->getInformacionReferenciaTag($general_data);
+    $children[] = $this->getNormativaTag($general_data);
+
+    if (!empty($general_data['others'])) {
+      $children[] = $this->getOtrosTag($general_data);
+    }
+    foreach ($children as $node) {
+      $root->appendChild($node);
+    }
+
+    $this->xml->appendChild($root);
+    $this->xml->save($this->file);
   }
 
   protected function initRootTag() {
@@ -34,7 +56,19 @@ class E_envoice_cr_TE_generator extends E_envoice_cr_document_generator {
   }
 
   protected function getInformacionReferenciaTag(&$data) {
-    
+    $tag = $this->xml->createElement('InformacionReferencia');
+    $children = array();
+    $children[] = $this->getSimpleTag('TipoDoc', $data['document_code']);
+    $children[] = $this->getSimpleTag('Numero', $data['key']);
+    $children[] = $this->getSimpleTag('FechaEmision', $data['date']);
+    $children[] = $this->getSimpleTag('Codigo', '02');
+    $children[] = $this->getSimpleTag('Razon', 'a');
+
+    foreach ($children as $child) {
+      $tag->appendChild($child);
+    }
+
+    return $tag;
   }
 
 }
