85a5de1bd91c859731c8decaad8208f6de2f0507
Alonso Mora <a.mora@paralleldevs.com>
2018-03-08 09:44:43 -0600

    [PF-13]: Added model for handling invoice related documents

---
+++ application/third_party/e_envoice_cr/models/Eenvoicecrsaledocuments.php
@@ -0,0 +1,74 @@
+<?php
+
+if (!defined('BASEPATH'))
+  exit('No direct script access allowed');
+
+/**
+ * Description of Eenvoicecrdocument
+ *
+ * @author pdev
+ */
+class Eenvoicecrsaledocuments extends CI_Model {
+
+  public function exists($document_id) {
+    $this->db->from('eenvoicecr_sales_documents');
+    $this->db->where(array(
+      'document_id' => $document_id,
+    ));
+    return ($this->db->get()->num_rows() == 1);
+  }
+
+  public function get_all($sale_id = '', $document_key = '', $document_code = '', $document_status = '') {
+    $this->db->select('document_id,sale_id,document_key,document_code,document_status');
+    $this->db->from('eenvoicecr_sales_documents');
+    $this->db->where(array(
+      'sale_id' => $sale_id,
+      'document_key' => $document_key,
+      'document_code' => $document_code,
+      'document_status' => $document_status,
+    ));
+    $this->db->order_by('sale_id', 'document_code');
+    $query = $this->db->get();
+
+    return $query;
+  }
+
+  public function get($document_id) {
+    $this->db->select('eenvoicecr_sales_documents.*');
+    $this->db->from('eenvoicecr_sales_documents');
+    $this->db->where('id', $document_id);
+    $query = $this->db->get();
+
+    if ($query->num_rows() == 1) {
+      return $query->row();
+    }
+    else {
+      //Get empty base parent object, as $item_id is NOT an item
+      $document_obj = new stdClass();
+
+      //Get all the fields from items table
+      foreach ($this->db->list_fields('eenvoicecr_sales_documents') as $field) {
+        $document_obj->$field = '';
+      }
+
+      return $document_obj;
+    }
+  }
+
+  public function save(&$document_data, $document_id = FALSE) {
+    if (!$document_id || !$this->exists($document_id)) {
+      if ($this->db->insert('eenvoicecr_sales_documents', $document_data)) {
+        $document_data['document_id'] = $this->db->insert_id();
+
+        return TRUE;
+      }
+
+      return FALSE;
+    }
+
+    $this->db->where('document_id', $document_id);
+
+    return $this->db->update('eenvoicecr_sales_documents', $document_data);
+  }
+
+}
