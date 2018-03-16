333fa3ed8a9f9fa1254b31712f8e67d1775f0ab9
Alonso Mora <a.mora@paralleldevs.com>
2018-03-16 10:00:10 -0600

    [PF-12]: Applied code styling. Refactor of login method

--- application/third_party/e_envoice_cr/config/Hacienda_constants.php
+++ application/third_party/e_envoice_cr/config/Hacienda_constants.php
@@ -1,7 +1,8 @@
 <?php
 
-if (!defined('BASEPATH')) {
-  exit('No direct script access allowed');
+if (!defined('BASEPATH'))
+{
+	exit('No direct script access allowed');
 }
 
 /**
@@ -11,71 +12,74 @@ if (!defined('BASEPATH')) {
  */
 class Hacienda_constants {
 
-  const ID_TYPE_PHYSICAL_PERSON_ID = "01";
-  const ID_TYPE_COMPANY_ID = "02";
-  const ID_TYPE_DIMEX_ID = "03";
-  const ID_TYPE_NITE_ID = "04";
-  const DOCUMENT_TYPE_FE = "FE";
-  const DOCUMENT_TYPE_TE = "TE";
-  const DOCUMENT_TYPE_NC = "NC";
-  const DOCUMENT_TYPE_ND = "ND";
-  const ENVIRONMENT_TYPE_PROD = "1";
-  const ENVIRONMENT_TYPE_STAG = "2";
-  const API_URL_PROD = 'https://api.comprobanteselectronicos.go.cr/recepcion/v1/';
-  const API_URL_STAG = 'https://api.comprobanteselectronicos.go.cr/recepcion-sandbox/v1/';
-  const AUTH_URL_PROD = "https://idp.comprobanteselectronicos.go.cr/auth/realms/rut/protocol/openid-connect/token";
-  const AUTH_URL_STAG = 'https://idp.comprobanteselectronicos.go.cr/auth/realms/rut-stag/protocol/openid-connect/token';
-  const AUTH_CLIENT_PROD = "api-prod";
-  const AUTH_CLIENT_STAG = "api-stag";
-  const DOCUMENT_TYPE_CODE_FE = '01';
-  const DOCUMENT_TYPE_CODE_ND = '02';
-  const DOCUMENT_TYPE_CODE_NC = '03';
-  const DOCUMENT_TYPE_CODE_TE = '04';
-  const XMLNS_FE = 'https://tribunet.hacienda.go.cr/docs/esquemas/2017/v4.2/facturaElectronica';
-  const XMLNS_TE = 'https://tribunet.hacienda.go.cr/docs/esquemas/2017/v4.2/tiqueteElectronico';
-  const XMLNS_NC = 'https://tribunet.hacienda.go.cr/docs/esquemas/2017/v4.2/notaCreditoElectronica';
-  const XMLNS_ND = 'https://tribunet.hacienda.go.cr/docs/esquemas/2017/v4.2/notaDebitoElectronica';
-  const TARGET_NAMESPACE_FE = 'https://tribunet.hacienda.go.cr/docs/esquemas/2017/v4.2/tiqueteElectronico';
-  const TARGET_NAMESPACE_TE = 'https://tribunet.hacienda.go.cr/docs/esquemas/2017/v4.2/tiqueteElectronico';
-  const TARGET_NAMESPACE_NC = 'https://tribunet.hacienda.go.cr/docs/esquemas/2017/v4.2/notaCreditoElectronica';
-  const TARGET_NAMESPACE_ND = 'https://tribunet.hacienda.go.cr/docs/esquemas/2017/v4.2/notaDebitoElectronica';
+	const ID_TYPE_PHYSICAL_PERSON_ID = "01";
+	const ID_TYPE_COMPANY_ID = "02";
+	const ID_TYPE_DIMEX_ID = "03";
+	const ID_TYPE_NITE_ID = "04";
+	const DOCUMENT_TYPE_FE = "FE";
+	const DOCUMENT_TYPE_TE = "TE";
+	const DOCUMENT_TYPE_NC = "NC";
+	const DOCUMENT_TYPE_ND = "ND";
+	const ENVIRONMENT_TYPE_PROD = "1";
+	const ENVIRONMENT_TYPE_STAG = "2";
+	const API_URL_PROD = 'https://api.comprobanteselectronicos.go.cr/recepcion/v1/';
+	const API_URL_STAG = 'https://api.comprobanteselectronicos.go.cr/recepcion-sandbox/v1/';
+	const AUTH_URL_PROD = "https://idp.comprobanteselectronicos.go.cr/auth/realms/rut/protocol/openid-connect";
+	const AUTH_URL_STAG = 'https://idp.comprobanteselectronicos.go.cr/auth/realms/rut-stag/protocol/openid-connect';
+	const AUTH_CLIENT_PROD = "api-prod";
+	const AUTH_CLIENT_STAG = "api-stag";
+	const DOCUMENT_TYPE_CODE_FE = '01';
+	const DOCUMENT_TYPE_CODE_ND = '02';
+	const DOCUMENT_TYPE_CODE_NC = '03';
+	const DOCUMENT_TYPE_CODE_TE = '04';
+	const XMLNS_FE = 'https://tribunet.hacienda.go.cr/docs/esquemas/2017/v4.2/facturaElectronica';
+	const XMLNS_TE = 'https://tribunet.hacienda.go.cr/docs/esquemas/2017/v4.2/tiqueteElectronico';
+	const XMLNS_NC = 'https://tribunet.hacienda.go.cr/docs/esquemas/2017/v4.2/notaCreditoElectronica';
+	const XMLNS_ND = 'https://tribunet.hacienda.go.cr/docs/esquemas/2017/v4.2/notaDebitoElectronica';
+	const TARGET_NAMESPACE_FE = 'https://tribunet.hacienda.go.cr/docs/esquemas/2017/v4.2/tiqueteElectronico';
+	const TARGET_NAMESPACE_TE = 'https://tribunet.hacienda.go.cr/docs/esquemas/2017/v4.2/tiqueteElectronico';
+	const TARGET_NAMESPACE_NC = 'https://tribunet.hacienda.go.cr/docs/esquemas/2017/v4.2/notaCreditoElectronica';
+	const TARGET_NAMESPACE_ND = 'https://tribunet.hacienda.go.cr/docs/esquemas/2017/v4.2/notaDebitoElectronica';
 
-  public static function get_id_types() {
-    return [
-      self::ID_TYPE_PHYSICAL_PERSON_ID => "Physical person id",
-      self::ID_TYPE_COMPANY_ID => "Company id",
-      self::ID_TYPE_DIMEX_ID => "DIMEX",
-      self::ID_TYPE_NITE_ID => "NITE",
-    ];
-  }
+	public static function get_id_types()
+	{
+		return [
+			self::ID_TYPE_PHYSICAL_PERSON_ID => "Physical person id",
+			self::ID_TYPE_COMPANY_ID => "Company id",
+			self::ID_TYPE_DIMEX_ID => "DIMEX",
+			self::ID_TYPE_NITE_ID => "NITE",
+		];
+	}
 
-  public static function get_environments() {
-    return [
-      self::ENVIRONMENT_TYPE_PROD => "Production",
-      self::ENVIRONMENT_TYPE_STAG => "Sandbox",
-    ];
-  }
+	public static function get_environments()
+	{
+		return [
+			self::ENVIRONMENT_TYPE_PROD => "Production",
+			self::ENVIRONMENT_TYPE_STAG => "Sandbox",
+		];
+	}
 
-  public static function get_code_by_document_type($type) {
-    $code = '';
-    switch ($type) {
-      case self::DOCUMENT_TYPE_FE:
-        $code .= self::DOCUMENT_TYPE_CODE_FE;
-        break;
-      case self::DOCUMENT_TYPE_TE:
-        $code .= self::DOCUMENT_TYPE_CODE_TE;
-        break;
-      case self::DOCUMENT_TYPE_NC:
-        $code .= self::DOCUMENT_TYPE_CODE_NC;
-        break;
-      case self::DOCUMENT_TYPE_ND:
-        $code .= self::DOCUMENT_TYPE_CODE_ND;
-        break;
-      default:
-        $code .= '00';
-        break;
-    }
-    return $code;
-  }
+	public static function get_code_by_document_type($type)
+	{
+		$code = '';
+		switch ($type) {
+			case self::DOCUMENT_TYPE_FE:
+				$code .= self::DOCUMENT_TYPE_CODE_FE;
+				break;
+			case self::DOCUMENT_TYPE_TE:
+				$code .= self::DOCUMENT_TYPE_CODE_TE;
+				break;
+			case self::DOCUMENT_TYPE_NC:
+				$code .= self::DOCUMENT_TYPE_CODE_NC;
+				break;
+			case self::DOCUMENT_TYPE_ND:
+				$code .= self::DOCUMENT_TYPE_CODE_ND;
+				break;
+			default:
+				$code .= '00';
+				break;
+		}
+		return $code;
+	}
 
 }
--- application/third_party/e_envoice_cr/libraries/E_envoice_cr_communicator.php
+++ application/third_party/e_envoice_cr/libraries/E_envoice_cr_communicator.php
@@ -1,7 +1,7 @@
 <?php
 
 if (!defined('BASEPATH'))
-  exit('No direct script access allowed');
+	exit('No direct script access allowed');
 
 require_once dirname(__DIR__) . '/config/Hacienda_constants.php';
 require_once dirname(__DIR__) . '/vendor/autoload.php';
@@ -16,189 +16,221 @@ use GuzzleHttp\RequestOptions;
  */
 class E_envoice_cr_communicator {
 
-  private $_ci;
-  private $_message;
-  private $_document_status;
-  private $_document_url;
-  private $_auth_token;
+	private $_ci;
+	private $_message;
+	private $_document_status;
+	private $_document_url;
+	private $_auth_token;
 
-  public function __construct() {
-    $this->_ci = & get_instance();
-    $this->_ci->load->add_package_path(APPPATH . 'third_party/e_envoice_cr/');
-    $this->_ci->load->model('Appconfig');
+	public function __construct()
+	{
+		$this->_ci = & get_instance();
+		$this->_ci->load->add_package_path(APPPATH . 'third_party/e_envoice_cr/');
+		$this->_ci->load->model('Appconfig');
 
-    $this->_message = '';
-    $this->_document_status = '';
-    $this->_auth_token = '';
-  }
+		$this->_message = '';
+		$this->_document_status = '';
+		$this->_auth_token = '';
+	}
 
-  public function getURLDocument() {
-    return $this->_document_url;
-  }
+	public function getURLDocument()
+	{
+		return $this->_document_url;
+	}
 
-  public function getErrorMessage() {
-    return $this->_message;
-  }
+	public function getErrorMessage()
+	{
+		return $this->_message;
+	}
 
-  public function getStatus() {
-    return $this->_document_status;
-  }
+	public function getStatus()
+	{
+		return $this->_document_status;
+	}
 
-  public function sendDocument(&$document_info, $xml_file) {
-    $this->login();
-    $url = $this->getAPIUrl() . 'recepcion';
-    $client = $this->getConnectionClient();
-    $data = $this->getDocumentPayload($document_info, $xml_file);
-    try {
-      $response = $client->post($url, [
-        RequestOptions::JSON => $data,
-      ]);
-      $this->processSendDocumentResponse($response);
-    }
-    catch (GuzzleHttp\Exception\RequestException $e_req) {
-      $this->_message = $e_req->getMessage();
-      $this->_document_status = 'Pendiente';
-      $this->_document_url = '';
-    }
-  }
+	public function sendDocument(&$document_info, $xml_file)
+	{
+		$this->login();
+		$url = $this->getAPIUrl() . 'recepcion';
+		$client = $this->getConnectionClient();
+		$data = $this->getDocumentPayload($document_info, $xml_file);
+		try
+		{
+			$response = $client->post($url, [
+				RequestOptions::JSON => $data,
+			]);
+			$this->processSendDocumentResponse($response);
+		}
+		catch (GuzzleHttp\Exception\RequestException $e_req)
+		{
+			$this->_message = $e_req->getMessage();
+			$this->_document_status = 'Pendiente';
+			$this->_document_url = '';
+		}
+	}
 
-  protected function processSendDocumentResponse(&$response) {
-    $code = $response->getStatusCode();
-    switch ($code) {
-      case 201:
-      case 202:
-        $url = $response->getHeader('Location');
-        $this->_message = 'Enviado';
-        $this->_document_status = 'Enviado';
-        $this->_document_url = implode(' ', $url);
-        break;
-      case 400:
-        $message = $response->getHeader('X-Error-Cause');
-        $this->_message = implode('. ', $message);
-        $this->_document_status = 'Invalido';
-        $this->_document_url = '';
-        break;
-      default :
-        break;
-    }    
-  }
+	protected function processSendDocumentResponse(&$response)
+	{
+		$code = $response->getStatusCode();
+		switch ($code) {
+			case 201:
+			case 202:
+				$url = $response->getHeader('Location');
+				$this->_message = 'Enviado';
+				$this->_document_status = 'Enviado';
+				$this->_document_url = implode(' ', $url);
+				break;
+			case 400:
+				$message = $response->getHeader('X-Error-Cause');
+				$this->_message = implode('. ', $message);
+				$this->_document_status = 'Invalido';
+				$this->_document_url = '';
+				break;
+			case 401:
+			case 403:
+			case 429:
+				$this->_document_status = 'Pendiente';
+				$this->_document_url = '';
+				break;
+			default :
+				break;
+		}
+	}
 
-  /**
-   * It gets the connection token.
-   */
-  protected function login() {
-    $username = $this->_ci->Appconfig->get('e_envoice_cr_username');
-    $password = $this->_ci->Appconfig->get('e_envoice_cr_password');
-    $url = $this->getAuthenticationURL();
-    $client_id = $this->getAuthenticationClientId();
+	/**
+	 * It gets the connection token.
+	 */
+	protected function login()
+	{
+		$username = $this->_ci->Appconfig->get('e_envoice_cr_username');
+		$password = $this->_ci->Appconfig->get('e_envoice_cr_password');
+		$url = $this->getAuthenticationURL();
+		$url .= '/token';
+		$client_id = $this->getAuthenticationClientId();
 
-    if ($username !== "" && $password !== "") {
-      $options = $this->getAuthenticationOptions($username, $password, $client_id);
-      $context = stream_context_create($options);
-      $result = file_get_contents($url, FALSE, $context);
-      if ($result === FALSE) {
-        echo $result;
-      }
+		if ($username !== "" && $password !== "")
+		{
+			$options = $this->getAuthenticationOptions($username, $password, $client_id);
+			$context = stream_context_create($options);
+			$result = file_get_contents($url, FALSE, $context);
+			if ($result === FALSE)
+			{
+				echo $result;
+			}
 
-      // Get a token object.
-      $token = json_decode($result);
-      // Return a json object whith token and refresh token.
-      $this->_auth_token = $token->access_token;
-    }
-    else {
-      $this->_auth_token = "";
-    }
-    return $this->_auth_token;
-  }
+			// Get a token object.
+			$token = json_decode($result);
+			// Return a json object whith token and refresh token.
+			$this->_auth_token = $token->access_token;
+		}
+		else
+		{
+			$this->_auth_token = "";
+		}
+		return $this->_auth_token;
+	}
 
-  protected function getAuthenticationURL() {
-    $environment = $this->_ci->Appconfig->get('e_envoice_cr_env');
-    if ($environment === Hacienda_constants::ENVIRONMENT_TYPE_PROD) {
-      $url = Hacienda_constants::AUTH_URL_PROD;
-    }
-    else {
-      $url = Hacienda_constants::AUTH_URL_STAG;
-    }
-    return $url;
-  }
+	protected function getAuthenticationURL()
+	{
+		$environment = $this->_ci->Appconfig->get('e_envoice_cr_env');
+		if ($environment === Hacienda_constants::ENVIRONMENT_TYPE_PROD)
+		{
+			$url = Hacienda_constants::AUTH_URL_PROD;
+		}
+		else
+		{
+			$url = Hacienda_constants::AUTH_URL_STAG;
+		}
+		return $url;
+	}
 
-  protected function getAuthenticationClientId() {
-    $environment = $this->_ci->Appconfig->get('e_envoice_cr_env');
-    if ($environment === Hacienda_constants::ENVIRONMENT_TYPE_PROD) {
-      $client_id = Hacienda_constants::AUTH_CLIENT_PROD;
-    }
-    else {
-      $client_id = Hacienda_constants::AUTH_CLIENT_STAG;
-    }
-    return $client_id;
-  }
+	protected function getAuthenticationClientId()
+	{
+		$environment = $this->_ci->Appconfig->get('e_envoice_cr_env');
+		if ($environment === Hacienda_constants::ENVIRONMENT_TYPE_PROD)
+		{
+			$client_id = Hacienda_constants::AUTH_CLIENT_PROD;
+		}
+		else
+		{
+			$client_id = Hacienda_constants::AUTH_CLIENT_STAG;
+		}
+		return $client_id;
+	}
 
-  protected function getAuthenticationOptions(&$username, &$password, &$client_id) {
-    $data = [
-      'client_id' => $client_id,
-      'client_secret' => '',
-      'grant_type' => 'password',
-      'username' => $username,
-      'password' => $password,
-      'scope' => '',
-    ];
-    // Use key 'http' even if you send the request to https://.
-    $options = [
-      'http' => [
-        'header' => "Content-type: application/x-www-form-urlencoded\r\n",
-        'method' => 'POST',
-        'content' => http_build_query($data),
-      ],
-    ];
-    return $options;
-  }
+	protected function getAuthenticationOptions(&$username, &$password, &$client_id)
+	{
+		$data = [
+			'client_id' => $client_id,
+			'client_secret' => '',
+			'grant_type' => 'password',
+			'username' => $username,
+			'password' => $password,
+			'scope' => '',
+		];
+		// Use key 'http' even if you send the request to https://.
+		$options = [
+			'http' => [
+				'header' => "Content-type: application/x-www-form-urlencoded\r\n",
+				'method' => 'POST',
+				'content' => http_build_query($data),
+			],
+		];
+		return $options;
+	}
 
-  protected function getConnectionClient() {
-    $client = new Client([
-      'http_errors' => false,
-      'connect_timeout' => 5,
-      'timeout' => 15,
-      'headers' => [
-        'Authorization' => 'Bearer ' . $this->_auth_token,
-        'Accept' => 'application/json',
-      ],
-    ]);
-    return $client;
-  }
+	protected function getConnectionClient()
+	{
+		$client = new Client([
+			'http_errors' => false,
+			'connect_timeout' => 5,
+			'timeout' => 15,
+			'headers' => [
+				'Authorization' => 'Bearer ' . $this->_auth_token,
+				'Accept' => 'application/json',
+			],
+		]);
+		return $client;
+	}
 
-  protected function getDocumentPayload(&$document_info, $xml_file) {
-    $document_content = file_get_contents($xml_file);
+	protected function getDocumentPayload(&$document_info, $xml_file)
+	{
+		$document_content = file_get_contents($xml_file);
 
-    $data = [
-      'clave' => $document_info['key'],
-      'fecha' => $document_info['date'],
-      'emisor' => [
-        'tipoIdentificacion' => $document_info['emitter']['id_type'],
-        'numeroIdentificacion' => $document_info['emitter']['id_number'],
-      ],
-    ];
+		$data = [
+			'clave' => $document_info['key'],
+			'fecha' => $document_info['date'],
+			'emisor' => [
+				'tipoIdentificacion' => $document_info['emitter']['id_type'],
+				'numeroIdentificacion' => $document_info['emitter']['id_number'],
+			],
+		];
 
-    if (!empty($document_info['receiver'])) {
-      $data['receptor'] = [
-        'tipoIdentificacion' => $document_info['receiver']['id_type'],
-        'numeroIdentificacion' => $document_info['receiver']['id_number'],
-      ];
-    }
+		if (!empty($document_info['receiver']))
+		{
+			$data['receptor'] = [
+				'tipoIdentificacion' => $document_info['receiver']['id_type'],
+				'numeroIdentificacion' => $document_info['receiver']['id_number'],
+			];
+		}
 
-    $data['comprobanteXml'] = base64_encode($document_content);
+		$data['comprobanteXml'] = base64_encode($document_content);
 
-    return $data;
-  }
+		return $data;
+	}
 
-  protected function getAPIUrl() {
-    $environment = $this->_ci->Appconfig->get('e_envoice_cr_env');
-    if ($environment === Hacienda_constants::ENVIRONMENT_TYPE_PROD) {
-      $url = Hacienda_constants::API_URL_PROD;
-    }
-    else {
-      $url = Hacienda_constants::API_URL_STAG;
-    }
-    return $url;
-  }
+	protected function getAPIUrl()
+	{
+		$environment = $this->_ci->Appconfig->get('e_envoice_cr_env');
+		if ($environment === Hacienda_constants::ENVIRONMENT_TYPE_PROD)
+		{
+			$url = Hacienda_constants::API_URL_PROD;
+		}
+		else
+		{
+			$url = Hacienda_constants::API_URL_STAG;
+		}
+		return $url;
+	}
 
 }
